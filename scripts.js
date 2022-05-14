cdb8baaf363054c11f184e08bafaabc76549fe9d
Gustavo <gustavo.leste@hotmail.com>
2022-05-14 16:11:42 -0300

    updateV2

--- scripts.js
+++ scripts.js
@@ -1,15 +1,9 @@
-const num1 = document.getElementById('num1')
-const num2 = document.getElementById('num2')
-let sinal = document.getElementById('selesoma')
-const button = document.getElementById('btnC')
 const text = document.getElementById('total')
-const buttonR = document.getElementById('btnR')
-const buttonN = document.getElementById('btnN')
 
-button.addEventListener('click', (cal1, cal2, expoent) =>{
+btnC.addEventListener('click', (cal1, cal2, expoent) =>{
     cal1 = Number(num1.value)
     cal2 = Number(num2.value)
-    expoent = sinal.value
+    expoent = selesoma.value
 
     if (cal1 == 0) {
         text.textContent = `Insira um valor no primeiro input!`
@@ -17,55 +11,51 @@ button.addEventListener('click', (cal1, cal2, expoent) =>{
     } else if (cal2 == 0) {
         text.textContent = `Insira um valor no segundo input!`
         num2.focus()
-    }else
-
-    
-    
-    switch (expoent) {
-        case 'soma':
-            result = cal1 + cal2
-            text.textContent = `${result}`
-         break;
-        
-        case 'divs':
-            result = cal1 / cal2
-            text.textContent = `${result}`
-        break;
-
-        case 'multi':
-            result = cal1 * cal2
-            text.textContent = `${result}`
-        break;
-
-        case 'menor':
-            result = cal1 - cal2
-            text.textContent = `${result}`
-        default:
+    }else {
+        switch (expoent) {
+            case 'soma':
+                result = cal1 + cal2
+                text.textContent = `${result}`
+             break;
+            
+            case 'divs':
+                result = cal1 / cal2
+                text.textContent = `${result}`
             break;
+    
+            case 'multi':
+                result = cal1 * cal2
+                text.textContent = `${result}`
+            break;
+    
+            case 'menor':
+                result = cal1 - cal2
+                text.textContent = `${result}`
+            default:
+                break;
+        }
     }
 
     if (result >= 1) {
-        buttonN.classList.remove('desativado')
-        buttonN.classList.add('ativo')
+        btnN.classList.remove('desativado')
+        btnN.classList.add('ativo')
     }else if (result <= -1) {
-        buttonN.classList.remove('desativado')
-        buttonN.classList.add('ativo')
-    }else {}
+        btnN.classList.remove('desativado')
+        btnN.classList.add('ativo')
+    }
 })
 
-
-buttonR.addEventListener('click', () =>{
+btnR.addEventListener('click', () =>{
     num1.value = 0
     num2.value = 0
     text.textContent = ''
     num1.focus()
-    buttonN.classList.remove('ativo')
-    buttonN.classList.add('desativado')
+    btnN.classList.remove('ativo')
+    btnN.classList.add('desativado')
     result = 0
 })
 
-
-buttonN.addEventListener('click', () => {
+btnN.addEventListener('click', () => {
     num1.value = result
-    num1.focus()
+    num2.focus()
 })
