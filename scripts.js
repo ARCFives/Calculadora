9183c840c03cc9fe50376ab41485e0c05fcd146d
Gustavo <gustavo.leste@hotmail.com>
2022-07-20 14:57:13 -0300

    Update V3
    Refatoração do código JS, e cores do CSS.

--- scripts.js
+++ scripts.js
@@ -1,77 +1,52 @@
 const text = document.getElementById('total')
 let modedark
 let toggleTheme = true
-let change = document.querySelectorAll('#base, #title, #num1, #num2, #selesoma, #btnR, #total, .autor, a')
+let change = document.querySelectorAll(
+  '#base, #title, #num1, #num2, #selesoma, #btnR, #total, .autor, a'
+)
 
 btnDark.addEventListener('click', () => {
-    if (toggleTheme) {
-        change.forEach(items => {items.classList.add('darkmode')})
-        document.body.classList.add('darkmode')
-        modedark = document.querySelectorAll('.darkmode')
-        btnDark.innerHTML = 'Light mode'
-    }else {
-        modedark.forEach(item => {item.classList.remove('darkmode')})
-        btnDark.innerHTML = 'Dark mode'
-    }
-    toggleTheme = !toggleTheme
+  if (toggleTheme) {
+    btnDark.innerHTML = 'Light mode'
+  } else {
+    btnDark.innerHTML = 'Dark mode'
+  }
+  change.forEach(item => item.classList.toggle('darkmode'))
+  document.body.classList.toggle('darkmode')
+  toggleTheme = !toggleTheme
 })
 
-btnC.addEventListener('click', (cal1, cal2, expoent) =>{
-    cal1 = Number(num1.value)
-    cal2 = Number(num2.value)
-    expoent = selesoma.value
+btnC.addEventListener('click', (cal1, cal2, expoent) => {
+  cal1 = Number(num1.value)
+  cal2 = Number(num2.value)
+  expoent = selesoma.value
 
-    if (cal1 == 0) {
-        text.textContent = `Insira um valor no primeiro input!`
-        num1.focus()
-        return
-    } else if (cal2 == 0) {
-        text.textContent = `Insira um valor no segundo input!`
-        num2.focus()
-        return
-    }else {
-        switch (expoent) {
-            case 'soma':
-                result = cal1 + cal2
-                text.textContent = `${result}`
-             break;
-            
-            case 'divs':
-                result = cal1 / cal2
-                text.textContent = `${result}`
-            break;
-    
-            case 'multi':
-                result = cal1 * cal2
-                text.textContent = `${result}`
-            break;
-    
-            case 'menor':
-                result = cal1 - cal2
-                text.textContent = `${result}`
-            default:
-                break;
-        }
-    }
+  if (cal1 == '' || cal2 == '') {
+    text.textContent = `Valores incorretos!`
+    return
+  }
 
-    if (result >= 1) {
-        btnN.style.display = 'block'
-    }else if (result <= -1) {
-        btnN.style.display = 'block'
-    }    
-})
+  result = eval(cal1 + expoent + cal2)
+  text.textContent = `${result}`
 
-btnR.addEventListener('click', () =>{
-    num1.value = ''
-    num2.value = ''
-    text.textContent = ''
-    num1.focus()
+  if (result >= 1 || result <= -1) {
+    btnN.style.display = 'block'
+  } else {
     btnN.style.display = 'none'
-    result = 0
+  }
+})
+
+btnR.addEventListener('click', () => {
+  num1.value = ''
+  num2.value = ''
+  text.textContent = ''
+  num1.focus()
+  btnN.style.display = 'none'
+  result = 0
 })
 
 btnN.addEventListener('click', () => {
-    num1.value = result
-    num2.value= ''
-    num2.focus()
+  num1.value = result
+  num2.value = ''
+  num2.focus()
 })
