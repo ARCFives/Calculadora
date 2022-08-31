b6c315afb95c881bb3dfd6ace10459cd78c1f951
Gustavo Vieira <gustavo.leste@hotmail.com>
2022-08-31 14:15:12 -0300

    v3.1.1
    * bugfix;
    * Increment delete button.

--- scripts.js
+++ scripts.js
@@ -6,48 +6,63 @@ const arithmetic = document.querySelectorAll('.ari')
 let soma = []
 
 numeros.forEach(num => {
-  num.addEventListener('click', (e) => {
+  num.addEventListener('click', e => {
     const numrsValue = e.target.value
+    if(display.value.includes('.') && numrsValue === '.'){
+      return
+    }
     display.value += numrsValue
   })
 })
 
 arithmetic.forEach(ari => {
-  ari.addEventListener('click', (expo) => {
-    
-    if(soma.length == ''){
-      if(display.value == ''){
+  ari.addEventListener('click', expo => {
+    if (soma.length == '') {
+      if (display.value == '') {
         return
-      }else{
+      } else {
         soma.push(display.value)
         display.value = ''
         const expoent = expo.target.value
         soma.push(expoent)
       }
-    }else{
+    } else {
       calculo()
-    }})})
+    }
+  })
+})
 
 btnReset.addEventListener('click', () => {
   display.value = ''
-  soma= []
+  soma = []
 })
 
 result.addEventListener('click', () => {
   calculo()
 })
 
+btnDelete.addEventListener('click', () => {
+  if (display.value == '') {
+    return
+  }else {
+    let currentValue = display.value
+    console.log(currentValue.slice(0, -1))
+    currentValue = currentValue.slice(0, -1)
+    display.value = currentValue
+  }
+})
+
 function calculo() {
-  if(display.value == ''){
-    if(soma.length == ''){
+  if (display.value == '') {
+    if (soma.length == '') {
       return
     }
-  }else if(soma.length == ''){
+  } else if (soma.length == '') {
     return
-  }else{
+  } else {
     soma.push(display.value)
     const resultado = eval(soma[0] + soma[1] + soma[2])
     display.value = resultado
-    soma= []
+    soma = []
   }
 }
